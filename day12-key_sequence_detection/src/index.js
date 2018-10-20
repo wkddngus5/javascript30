2241ce37c05033959fc8c91d361d9cb15e272dc3
wkddngus5 <wkddngus5@naver.com>
2018-10-20 22:16:04 +0900

    day12/Key-Sequence-Detection

---
+++ day12-key_sequence_detection/src/index.js
@@ -0,0 +1,15 @@
+import style from './main.css';
+
+const pressed = [];
+const secretCode = 'secrete';
+
+window.addEventListener('keyup', (e) => {
+  console.log(e.key);
+  pressed.push(e.key);
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+  if (pressed.join('').includes(secretCode)) {
+    console.log('DING DING!');
+    cornify_add();
+  }
+  console.log(pressed);
+});
